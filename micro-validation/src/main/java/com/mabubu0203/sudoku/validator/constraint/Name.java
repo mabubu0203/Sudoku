989787d3be55e88e6bc20058b425faed99009364
uratamanabu <mabu_ip@msn.com>
2019-03-13 17:50:01 +0900

    #7 各種移植

---
+++ micro-validation/src/main/java/com/mabubu0203/sudoku/validator/constraint/Name.java
@@ -0,0 +1,20 @@
+package com.mabubu0203.sudoku.validator.constraint;
+
+import javax.validation.Payload;
+import javax.validation.constraints.Size;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.PARAMETER, ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Size(max = 64)
+public @interface Name {
+
+  String message() default "KeyHash is invalid";
+
+  Class<?>[] groups() default {};
+
+  Class<? extends Payload>[] payload() default {};
+}
---
+++ micro-validation/src/main/java/com/mabubu0203/sudoku/validator/constraint/Score.java
@@ -0,0 +1,20 @@
+package com.mabubu0203.sudoku.validator.constraint;
+
+import javax.validation.Payload;
+import javax.validation.constraints.Digits;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.PARAMETER, ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Digits(integer = 7, fraction = 0)
+public @interface Score {
+
+  String message() default "Score is invalid";
+
+  Class<?>[] groups() default {};
+
+  Class<? extends Payload>[] payload() default {};
+}
