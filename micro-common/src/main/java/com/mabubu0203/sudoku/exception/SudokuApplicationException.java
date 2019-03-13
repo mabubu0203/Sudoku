c3417a11bd67ec3dafd3bb2e4ca04c4ad6e41ac8
uratamanabu <mabu_ip@msn.com>
2019-03-13 18:17:37 +0900

    #7 各種移植

---
+++ micro-common/src/main/java/com/mabubu0203/sudoku/exception/SudokuApplicationException.java
@@ -0,0 +1,14 @@
+package com.mabubu0203.sudoku.exception;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author uratamanabu
+ * @version 1.0
+ * @since 1.0
+ */
+@Slf4j
+public class SudokuApplicationException extends RuntimeException {
+
+  private static final long serialVersionUID = -4376900603436217304L;
+}
