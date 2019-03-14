ccc5e7540603bdc088804f6c9bebe05e4dff86fb
uratamanabu <mabu_ip@msn.com>
2019-03-14 21:33:01 +0900

    #11 各種移植

--- micro-web/src/main/java/com/mabubu0203/sudoku/web/WebApp.java
+++ micro-web/src/main/java/com/mabubu0203/sudoku/web/WebApp.java
@@ -1,7 +1,6 @@
 package jp.co.valtech.sudoku.web;
 
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import java.util.concurrent.Executor;
 import jp.co.valtech.sudoku.core.Core;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -16,6 +15,8 @@ import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
+import java.util.concurrent.Executor;
+
 /**
  * @author uratamanabu
  * @version 1.0
@@ -27,52 +28,52 @@ import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 @EnableScheduling
 public class WebApp extends SpringBootServletInitializer implements SchedulingConfigurer {
 
-  /**
-   * @author uratamanabu
-   * @version 1.0
-   * @since 1.0
-   */
-  public static void main(String[] args) {
-    new SpringApplicationBuilder(WebApp.class).profiles("dev").run(args);
-  }
-
-  @Override
-  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-    return builder.sources(WebApp.class);
-  }
-
-  /**
-   * @author uratamanabu
-   * @version 1.0
-   * @since 1.0
-   */
-  @Override
-  public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
-    taskRegistrar.setScheduler(taskScheduler());
-  }
-
-  /**
-   * @author uratamanabu
-   * @version 1.0
-   * @since 1.0
-   */
-  @Bean(destroyMethod = "shutdown")
-  public Executor taskScheduler() {
-    ThreadPoolTaskScheduler threadPool = new ThreadPoolTaskScheduler();
-    threadPool.setPoolSize(5);
-
-    // shutdown 時に Task の完了を待つ（最大10分間）
-    threadPool.setWaitForTasksToCompleteOnShutdown(true);
-    threadPool.setAwaitTerminationSeconds(60 * 10);
-    return threadPool;
-  }
-
-  @Configuration
-  public class JacksonConfiguration {
-
-    @Bean
-    public JavaTimeModule javaTimeModule() {
-      return new JavaTimeModule();
+    /**
+     * @author uratamanabu
+     * @version 1.0
+     * @since 1.0
+     */
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(WebApp.class).profiles("dev").run(args);
     }
-  }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(WebApp.class);
+    }
+
+    /**
+     * @author uratamanabu
+     * @version 1.0
+     * @since 1.0
+     */
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        taskRegistrar.setScheduler(taskScheduler());
+    }
+
+    /**
+     * @author uratamanabu
+     * @version 1.0
+     * @since 1.0
+     */
+    @Bean(destroyMethod = "shutdown")
+    public Executor taskScheduler() {
+        ThreadPoolTaskScheduler threadPool = new ThreadPoolTaskScheduler();
+        threadPool.setPoolSize(5);
+
+        // shutdown 時に Task の完了を待つ（最大10分間）
+        threadPool.setWaitForTasksToCompleteOnShutdown(true);
+        threadPool.setAwaitTerminationSeconds(60 * 10);
+        return threadPool;
+    }
+
+    @Configuration
+    public class JacksonConfiguration {
+
+        @Bean
+        public JavaTimeModule javaTimeModule() {
+            return new JavaTimeModule();
+        }
+    }
 }
