87ccd0ed6be099b169c2df70c7a44afde03e3418
uratamanabu <mabu_ip@msn.com>
2019-07-21 10:15:20 +0900

    #84 修正

--- micro-common/src/main/java/com/mabubu0203/sudoku/clients/rdb/RdbApiSearchEndPoints.java
+++ micro-common/src/main/java/com/mabubu0203/sudoku/clients/rdb/RdbApiSearchEndPoints.java
@@ -6,13 +6,19 @@ import com.mabubu0203.sudoku.interfaces.SearchConditionBean;
 import com.mabubu0203.sudoku.interfaces.domain.AnswerInfoTbl;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.Page;
-import org.springframework.http.*;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestOperations;
+import org.springframework.web.util.UriTemplate;
 
 import java.net.URI;
-import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * {@code /answerInfoTbls}のエンドポイントのラッパーです。<br>
@@ -36,29 +42,34 @@ public class RdbApiSearchEndPoints {
      */
     public Page<AnswerInfoTbl> search(
             final RestOperations restOperations,
-            final SearchConditionBean conditionBean) {
+            final SearchConditionBean conditionBean,
+            final Pageable pageable) {
+
         final String search = "http://localhost:9011/SudokuRdb/"
                 + CommonConstants.SLASH + RestUrlConstants.URL_SEARCH_MASTER + CommonConstants.SLASH;
-
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("selectType", "4");
+        uriVariables.put("selectorNo", Integer.toString(conditionBean.getSelectorNo()));
+        uriVariables.put("selectorKeyHash", Integer.toString(conditionBean.getSelectorKeyHash()));
+        uriVariables.put("selectorScore", Integer.toString(conditionBean.getSelectorScore()));
+        uriVariables.put("selectorName", Integer.toString(conditionBean.getSelectorName()));
+        uriVariables.put("number", Integer.toString(pageable.getPageNumber()));
+        uriVariables.put("size", Integer.toString(pageable.getPageSize()));
+        URI uri = new UriTemplate(search + "?type={type}").expand(uriVariables);
+        RequestEntity requestEntity =
+                RequestEntity
+                        .get(uri)
+                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_UTF8_VALUE)
+                        .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_UTF8_VALUE)
+                        .build();
         try {
-            URI uri = new URI(search);
-            RequestEntity requestEntity =
-                    RequestEntity
-                            .post(uri)
-                            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_UTF8_VALUE)
-                            .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_UTF8_VALUE)
-                            .body(search);
-            ResponseEntity<Page<AnswerInfoTbl>> generateEntity = restOperations.exchange(requestEntity, new ParameterizedTypeReference<Page<AnswerInfoTbl>>() {
-            });
-            HttpStatus status = generateEntity.getStatusCode();
-            switch (status) {
-                case OK:
-                    return generateEntity.getBody();
-                case CONFLICT:
-                default:
-                    return null;
-            }
-        } catch (URISyntaxException | RestClientException e) {
+            ResponseEntity<Page<AnswerInfoTbl>> generateEntity = restOperations.exchange(
+                    requestEntity,
+                    new ParameterizedTypeReference<>() {
+                    }
+            );
+            return generateEntity.getBody();
+        } catch (RestClientException e) {
             e.printStackTrace();
             return null;
         }
